2120c0b1896189edbb190a22c581c5defc75063a
clovett <zwehhett@gmail.com>
2024-07-07 21:20:22 +0300

    Remove options

--- generateImg.py
+++ generateImg.py
@@ -13,19 +13,13 @@ load_dotenv()
 chromedriver_path = getenv('CHROMEDRIVER_PATH')
 
 
-# Настройки для headless режима
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
 # Инициализация сервиса
 service = Service(chromedriver_path)
 
 
 def generateImg(prompt):
     # Инициализация браузера
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=service)
     driver.get('https://www.midgenai.com')
 
     # Даем странице загрузиться
