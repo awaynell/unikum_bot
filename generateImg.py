838f1a524c99057ec337394ed1b84073e79b8235
clovett <zwehhett@gmail.com>
2024-07-08 15:35:50 +0300

    New generation image model

--- generateImg.py
+++ generateImg.py
@@ -15,6 +15,8 @@ load_dotenv()
 
 chromedriver_path = getenv('CHROMEDRIVER_PATH')
 
+client = Client("prithivMLmods/IMAGINEO-4K")
+
 
 # Настройки для headless режима
 chrome_options = Options()
@@ -89,19 +91,17 @@ def generateImg(prompt):
 
 async def getImgFromAPI(prompt, update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        client = Client("mukaist/DALLE-4K")
 
         result = client.predict(
             prompt=prompt,
             negative_prompt="(deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation",
             use_negative_prompt=True,
-            style="3840 x 2160",
             seed=0,
             width=1024,
             height=1024,
             guidance_scale=6,
             randomize_seed=True,
-            api_name="/run"
+            api_name="/run",
         )
 
         print('result', result)
